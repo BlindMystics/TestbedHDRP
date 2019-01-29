ce8464cebb8dba95092e381d5046ef2f817b7eaa
P3TE <thisisntanemailaccount@gmail.com>
2019-01-29 16:39:34 +1000

    Upgraded to version 2018.3.3f1
    ^ The XR memory leak bug is still not fixed... REEE ^

---
+++ Assets/P3TE/StateMachineBehaviousTest/Scripts/StateMachineBehaviourTest1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachineBehaviourTest1 : StateMachineBehaviour {
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        Debug.Log("OnStateEnter: " + stateInfo.ToString());
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+        Debug.Log("OnStateExit: " + stateInfo.ToString());
+    }
+
+    public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateMove(animator, stateInfo, layerIndex);
+        Debug.Log("OnStateMove: " + stateInfo.ToString());
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+        Debug.Log("OnStateUpdate: " + stateInfo.ToString());
+    }
+}
